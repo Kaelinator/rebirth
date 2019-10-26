80b84347d2d6e75ca3801a11c053ada594e03e17
Shan Memon <shankmemon@gmail.com>
2019-10-26 12:40:40 -0500

    get animation working

--- client/animation.js
+++ client/animation.js
@@ -1,6 +1,31 @@
-function setup() {
-  createCanvas(500, 500);
-  background(255);
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
+let spritesheet
+let spritedata
+
+let animation = []
+
+function preload() {
+  spritedata = loadJSON('./assets/sprites/spritesheet_data/spritesheet.json')
+  spritesheet = loadImage('./assets/sprites/spritesheets/spritesheet.png')
 }
 
-function draw() {}
+function setup() {
+  createCanvas(500, 500)
+  background(255)
+
+  let images = Object.values(spritedata.frames)
+
+  images.forEach(({ frame }) => {
+    const { x, y, w, h } = frame
+    const img = spritesheet.get(x, y, w, h)
+    animation.push(img)
+  })
+
+}
+
+function draw() {
+  background(255)
+  image(animation[frameCount % animation.length], 0, 0)
+}
