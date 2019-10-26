59366982f684b16950aaf039a5615397e1a00025
Emilio <erogalla@utexas.edu>
2019-10-26 14:13:48 -0500

    starting physics

--- server/gameloop.js
+++ server/gameloop.js
@@ -1,6 +1,8 @@
 
 const WebSocket = require('ws')
 const uuid = require('uuid/v4')
+const JUMP_SPEED = 1
+const FALL_SPEED = 10
 
 const data = {
   players: {},
@@ -15,7 +17,11 @@ const configureWebSockets = server => {
   socketServer.on('connection', ws => {
 
     ws.id = uuid()
-    players[ws.id] = {}
+    players[ws.id] = {
+      position = createVector(x, y),
+      velocity = createVector(0, 0),
+      acceleration = createVector(0, 0),
+    }
     console.log(`new user: ${ws.id}`)
 
     ws.on('message', message => {
@@ -33,14 +39,42 @@ const configureWebSockets = server => {
 const startGameLoop = interval => {
   const { socketServer, players } = data
   setInterval(() => {
-    const updated = Object.values(players).map(changeVelocity)
+    const updated = Object.values(players).map(updatePos)
     emitToAll(socketServer, updated)
   }, interval)
 }
 
-const changeVelocity = player => ({
-  ...player
-})
+const updatePos = player => {
+  update(player);
+  
+  if(player.isJumping) {
+    player.velocity.y = JUMP_SPEED;
+  } else {
+    player.acceleration.add(createVector(0, FALL_SPEED)); 
+  }
+  
+  //   player.y = updateY(player)
+  // }
+  // if(player.strafeRight) {
+  //   player.x = updateX(player);
+  // } else if ()
+
+}
+return player;
+
+const update = player =>  {
+  // Velocity changes according to acceleration
+  player.velocity.add(player.acceleration);
+  // position changes by velocity
+  player.position.add(player.velocity);
+  // We must clear acceleration each frame
+  player.acceleration.mult(0);
+};
+
+
+// const updateY = player => {
+//   ...player
+// }
 
 
 const emitToAll = (socketServer, data) => {
