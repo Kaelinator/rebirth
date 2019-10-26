7363599a48f07dc496c291e73dc419bcd49c3094
Kaelinator <kirkkael@gmail.com>
2019-10-26 11:42:21 -0500

    configured eslint

--- client/game.js
+++ client/game.js
@@ -1,6 +1,11 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
 function setup() {
-  createCanvas(500, 500);
-  background(51);
+  createCanvas(500, 500)
+  background(51)
 }
 
-function draw() {}
+function draw() {
+  
+}
--- index.js
+++ index.js
@@ -2,13 +2,20 @@ const express = require('express')
 const WebSocket = require('ws')
 const http = require('http')
 const app = express()
-const path = require("path")
+const path = require('path')
 
-app.use("/", express.static(path.join(__dirname, "client")));
+app.use('/', express.static(path.join(__dirname, 'client')))
 
 const server = http.createServer(app)
 
-const socketServer = new WebSocket.Server({ server });
+const socketServer = new WebSocket.Server({ server })
+
+socketServer.on('connection', ws => {
+  console.log('new user')
+  ws.on('message', message => {
+    console.log(`message: ${message}`)
+  })
+})
 
 server.listen(process.env.PORT || 3000, () => {
   console.log(`WE'RE LIVE! ${server.address().port}`)
