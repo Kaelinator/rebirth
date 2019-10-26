719b0b9ed9edcf686dc877513b71f8faded09fd0
Kaelinator <kirkkael@gmail.com>
2019-10-26 11:31:39 -0500

    added linter

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const WebSocket = require('ws')
+const http = require('http')
 const app = express()
+
 
 app.get('/', (req, res) => {
   res.send('Testing')
 })
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('this is my change')
+const server = http.createServer(app)
+
+const socketServer = new WebSocket.Server({ server });
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log(`WE'RE LIVE! ${server.address().port}`)
 })
