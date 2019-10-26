1017a65744b8b444a65ebd560ab1e2365c6d7322
Shan Memon <shankmemon@gmail.com>
2019-10-26 11:30:50 -0500

    impl game.js and linked p5

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require('express')
-const app = express()
+const path = require("path");
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Testing')
-})
+app.use("/", express.static(path.join(__dirname, "client")));
+
+// app.get("/", (req, res) => {
+//   res.send("Testing");
+// });
 
 app.listen(3000, () => {
-  console.log('this is my change')
-})
+  console.log("this is my change");
+});
